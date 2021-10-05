cab7f658491505bf6d0782236ea0336066eccc09
RUKA5652 <ruka46497@gmail.com>
2021-10-06 00:51:13 +0900

    更新

--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -1,5 +1,6 @@
 #include<DxLib.h>
-
+#include"_debug/_DebugConOut.h"
+#include"_debug/_DebugDispOut.h"
 
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_	HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)//エントリーポイント 
@@ -7,12 +8,17 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_	HINSTANCE hPrevInstance, _
 	ChangeWindowMode(true);
 	SetMainWindowText("2016016_杉岡　竜二");
 	SetGraphMode(500, 500, 32);
+	_dbgSetup(500, 500, 255);			//255は透明度	
 	DxLib_Init();
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	while (ProcessMessage() == 0 && !CheckHitKey(KEY_INPUT_ESCAPE))
 	{
+		_dbgStartDraw();
+
+		SetDrawScreen(DX_SCREEN_BACK);
 		ClearDrawScreen();
+		_dbgAddDraw();
 		ScreenFlip();
 
 
